a2df3f72eed4493f1cbab307a1784db2f3954eaf
Nick Ottens <nickottens@mbp-van-nick.lan>
2021-07-15 11:19:12 +0200

    fix

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" value="{{ csrf_token() }}" />
 
-    <title>ADPI</title>
+    <title>Laravel/Vue CRUD-SYSTEM</title>
 
     <link href="{{ mix('css/app.css') }}" type="text/css" rel="stylesheet" />
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
